7e36a66079ff3d8ddf1eb5547548b87b664f4c2c
laiba345 <2846016855@qq.com>
2024-04-24 23:38:39 +0800

    react初次

---
+++ 07_书写JSX推荐的VS配置.md
@@ -0,0 +1,12 @@
+/**
+1. JS 文件中启用 Emmet 语法支持
+"emmet.includeLanguages": {
+"javascript": "javascriptreact”
+},
+2. 按照两个插件：
+- Prettier - Code formatter 自动格式化 JSX 代码
+- 保存时自动格式化代码： "editor.formatOnSave": true
+- 设置为默认格式化工具： "editor.defaultFormatter": "esbenp.prettier-vscode",
+- Auto Rename Tag 自动修改成对的标签名称
+- 无需额外配置
+*/
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,5 +1,6 @@
 // 1. 导包   react-dom 专门渲染到DOM浏览器当中；
 import { createRoot } from "react-dom/client";
+import './index.css'
 // 2. 创建 React 根对象
 const root = createRoot(document.querySelector("#root"));
 // 3. 使用根对象，渲染 React 内容
@@ -10,8 +11,35 @@ const root = createRoot(document.querySelector("#root"));
 // 1. 必须有一个根标签
 // 2. 所有标签必须闭合
 // 3. 标签属性名称必须是驼峰命名法 className
+
+// 列表渲染
+// 1. 数组的map方法
+// 2. 列表项要添加 key 属性, key的值要唯一
+
+// 条件渲染
+// 1. 逻辑与 &&
+//
+
+// 分类菜单数组
+const categories = [
+  { id: 1, name: "推荐" },
+  { id: 2, name: "一人西餐" },
+  { id: 3, name: "西贝凉菜" },
+  { id: 4, name: "西贝热菜" },
+  { id: 5, name: "杂粮主食" },
+];
+
 root.render(
   <div>
-    <h1>Hello React</h1>
+    <ul className="list">
+      {categories.map((item, index) => {
+        return (
+          <li key={item.id}>
+            {/* {index === 0 && <h1>请帮忙推荐内容</h1>} */}
+            {item.name}
+          </li>
+        );
+      })}
+    </ul>
   </div>
 );
